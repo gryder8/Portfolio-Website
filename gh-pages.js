ed7df594db2483159ac33c5f8bf8346cfce8bdab
gryder8 <gavin.ryder82@icloud.com>
2023-02-23 16:33:48 -0800

    Fix repo url for pages deployment script

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/gryder8/Portfolio-Website.git', // Update to point to your repository
+        repo: 'https://github.com/gryder8/gryder8.github.io.git', // Update to point to your repository
         user: {
             name: 'Gavin Ryder', // update to use your name
             email: 'gavin.ryder82@gmail.com' // Update to use your email
@@ -13,4 +13,6 @@ ghpages.publish(
     () => {
         console.log('Deploy Complete!')
     }
-)
+).then(() => {
+    console.log("Website published!")
+})
